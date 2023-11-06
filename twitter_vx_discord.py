7a851e8214abd4b33689d8e96ae7329af354aa1b
Theodore Stabile <stabilet327@strose.edu>
2023-11-06 15:37:00 -0500

    Update twitter_vx_discord.py
    Working in perpetuity via pythonanywhere

--- twitter_vx_discord.py
+++ twitter_vx_discord.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
 import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import re
 
+#This script runs the backend of the Jahnel Discord Bot.
+#It will check for any messages with a twitter.com or x.com URL and replace it with vxtwitter.com.
+#This restores embedded functionality
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-TOKEN = 'ask ted'
+#TOKEN = os.getenv('DISCORD_TOKEN')
+#GUILD = os.getenv('DISCORD_GUILD')
+print('Starting up Jahnel Bot')
+TOKEN = 'MTE3MDg5NzQ2MzgxNzc5NzY5Mw.G31Bra.8wU8OPxXSVpkFm5dDJNZoTj0E1lMMTIaOyDrDQ'
 print(TOKEN)
 
 # Define your bot's intents
@@ -24,13 +29,12 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     for guild in bot.guilds:
-        if guild.name == GUILD:
-            break
 
-    print(
+
+         print(
         f'{bot.user} is connected to the following serverss:\n'
         f'{guild.name}(id: {guild.id})'
-    )
+        )
 
 @bot.event
 async def on_message(message):
@@ -40,9 +44,10 @@ async def on_message(message):
     content = message.clean_content
 
     if is_twitter_url(message.content):
-        
-        if 'x.' in message.content:
+
+        if re.search(r'https?://(?:www\.)?x\.com/\S+/status/\d+',message.content): #x.com
             modified_message = content.replace('x.com', 'vxtwitter.com')
+
         else:
             modified_message = content.replace('twitter.com', 'vxtwitter.com')
 
@@ -53,13 +58,13 @@ async def on_message(message):
             await message.channel.send(f":index_pointing_at_the_viewer: TWEET :index_pointing_at_the_viewer: from **{message.author.name}** : {modified_message}")
 
 
-        print("did it change?")
-    
+
+
     await bot.process_commands(message)  # Process commands as well
 
 def is_twitter_url(content):
    # print("Content:",content)
-    print(re.search(r'https?://(?:www\.)?twitter\.com/\S+/status/\d+', content))
-    return re.search(r'https?://(?:www\.)?twitter\.com/\S+/status/\d+', content) or (r'https?://(?:www\.)?x\.com/\S+/status/\d+', content) 
+#    print(re.search(r'https?://(?:www\.)?twitter\.com/\S+/status/\d+', content))
+    return re.search(r'https?://(?:www\.)?twitter\.com/\S+/status/\d+', content) or re.search(r'https?://(?:www\.)?x\.com/\S+/status/\d+', content)
 
 bot.run(TOKEN)
